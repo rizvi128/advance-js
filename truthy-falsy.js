aebcd8753fdae63bc9c7d26aef985abcde94cb5e
rizvi128 <rizvi07854@gmail.com>
2020-11-26 21:58:54 +0600

    truth

---
+++ truthy-falsy.js
@@ -0,0 +1,20 @@
+//falsy
+//false
+//0
+//""
+//undifined
+//null
+//NuN
+
+//truthy
+//" "
+//"0"
+//[]
+//"false"
+let name = "false"
+if(name){
+    console.log("condition is truth");
+} 
+else {
+    console.log("condition is false");
+}
