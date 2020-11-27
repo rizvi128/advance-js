08a7669901ec2c21806673fa5b61f109149a996e
rizvi128 <rizvi07854@gmail.com>
2020-11-27 12:25:22 +0600

    double and tripple equal confusion is cleared

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,25 @@
+// const first = 0;
+// const second = false;
+// if(first == second)
+// {
+//     console.log(" this is true");
+
+// }
+// else{
+//     console.log("this is not true");
+// }
+const first = 0;
+const second = false;
+if(first === second)
+{
+    console.log(" this is true");
+
+}
+else{
+    console.log("this is false");
+}
+
+
+
+//its mean that when we will use the double equal then it will check that is the variable type is similar or not.
+//and if you use the tripple equal then it will check both the type and value.
