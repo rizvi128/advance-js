e626b6a26df8d720140ffd757677c21d23b7c063
rizvi128 <rizvi07854@gmail.com>
2020-11-27 11:39:28 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefine.js
@@ -0,0 +1,12 @@
+//let pakhi;
+function add(num1, num2){
+    console.log(num1);
+    return
+}
+const result = add(1, 2);
+console.log(result);
+
+let fun = undefined;
+console.log(fun);
+let ages = [2, 5, 3]
+console.log(ages[11]);
